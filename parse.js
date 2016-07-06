ca55119b5dad09226039e41e37d24c95e56467e2
ayanbhaduryfc <ayan.bhadury@fusioncharts.com>
2016-07-06 20:01:40 +0530

    task 3
    range calculation

--- parse.js
+++ parse.js
@@ -13,6 +13,9 @@ var chart2= new jparsing(json.xdata,json.xaxisname,json.dataset[1].yaxisname,jso
 console.log(chart2);
 var chart3= new jparsing(json.xdata,json.xaxisname,json.dataset[2].yaxisname,json.dataset[2].ydata);
 console.log(chart3);
+xaxisshow();
+range();
+
   }
 }
 xmlhttp.open('GET','master_data.json',true);
@@ -44,4 +47,31 @@ function fetchjparsing(){
   console.log(json.dataset[0].yaxisname);
   console.log(json.dataset[0].ydata[0]);
   //console,log(json["ydata"]);
+}
+
+function range(){
+  for(var k=0;k<3;k++){
+  var min= Math.min.apply(this,json.dataset[k].ydata);
+  console.log(min + " minimum value");//100
+  var max= Math.max.apply(this,json.dataset[k].ydata);
+  console.log(max+ " maximum value");//800
+//var distance=max-min;
+//var distance= Math.sqrt((Math.pow(max,2))-(Math.pow(min,2)));//793.011
+  //console.log(distance);
+  var jsonlength=json.dataset[k].ydata.length;
+  console.log(jsonlength+ " array length");//7
+  var divcalculate =Math.ceil((max/jsonlength)/10)*10;
+  console.log(divcalculate+ " per interval");
+    var j=min;
+  for(var i=0;i<json.dataset[k].ydata.length;i++){
+
+    console.log(j);
+    j = j+divcalculate;
+
+
+  }
+}
+}
+function xaxisshow(){
+  document.getElementById('xshow').innerHTML=json.xdata;
 }
